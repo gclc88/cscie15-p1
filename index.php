90ea951a6561ac737f450d338a6fbbcccef2aa36
Godfrey CHENG <gclc88@gmail.com>
2016-02-11 05:03:27 -0500

    Added index.php, same as profile.html

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>CSCIE-15 Project 1</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='css/main.css' rel='stylesheet'>
+
+</head>
+
+
+<body>
+
+	<div class="profile">
+
+		<h1>Godfrey CHENG</h1>
+		<img src='images/photo.jpg' alt='Godfrey CHENG'>
+	</div>
+	<div class="projects">
+		<h3>Project 2 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+		<h3>Project 3 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+		<h3>Project 4 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+
+
+	</div>
+
+</body>
+</html>
